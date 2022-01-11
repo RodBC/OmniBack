22eb06cd299b73ee70185f80ca011f658a40abc7
RodBC <rodrigo.barros.castro@hotmail.com>
2022-01-11 10:11:18 -0300

    using github API to request and store User data

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,28 @@
 const express = require("express");
 const axios = require("axios");
 const routes = express.Router();
+const Dev = require("./models/Dev")
+
 
 routes.post('/devs', async (req, res) => {
-    const {github_username} = req.body;
+    const {github_username, techs} = req.body;
 
+    const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
+    
+    const { name = login, avatar_url, bio } = apiResponse.data;
+   
 
-    const response = await axios.get(`https://api.github.com/users/${github_username}`);
-    console.log(response.data);
-    return res.json({message: "hello"})
+/*    const techsArray = techs.split(',').map(tech => tech.trim());
+
+    const dev = await Dev.create({
+        github_username,
+        name,
+        avatar_url,
+        bio,
+        techs: techsArray,
+    });
+*/
+    return res.json(apiResponse.data);
 });
 
 module.exports = routes;
